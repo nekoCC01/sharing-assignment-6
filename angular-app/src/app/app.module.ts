1798f9c6cb8356ade16edd0d54586d03b839d9eb
Daniel Kasai <danielkasai2012@gmail.com>
2018-04-14 17:16:48 +0200

    new sub-component for sentences

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SentenceComponent } from './sentence/sentence.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SentenceComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-app/src/app/sentence/sentence.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-sentence',
+    templateUrl: './sentence.component.html',
+    styleUrls: ['./sentence.component.css']
+})
+export class SentenceComponent implements OnInit {
+    @Input() sentences;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
